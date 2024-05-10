f1dcb6e2f9ea05221ce253e412d731cde02daeac
Paweł Ługowski <pawel.lugowski02@gmail.com>
2024-05-10 19:10:00 +0200

    Poprawiono wygląd navbara

--- YourTrainerApp2/Program.cs
+++ YourTrainerApp2/Program.cs
@@ -32,24 +32,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapControllerRoute(
-//        name: "default",
-//        pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//    endpoints.MapControllerRoute(
-//        name: "Admin",
-//        pattern: "{area:exists}/{controller=ExerciseAdmin}/{action=Index}/{id?}");
-//});
-
-
-
-//app.MapControllerRoute(
-//        name: "default",
-//        pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.MapControllerRoute(
         name: "default",
         pattern: "{area=Visitor}/{controller=Home}/{action=Index}/{id?}");
