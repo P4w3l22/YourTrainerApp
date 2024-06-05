128e9482c6ae9f74fdcbb95e4759e39503455514
Paweł Ługowski <pawel.lugowski02@gmail.com>
2024-06-05 12:56:33 +0200

    Przekazano model danych zawierający trainerdata do widoku wyboru trenera

--- YourTrainerApp2/Areas/GymMember/Controllers/TrainerContactController.cs
+++ YourTrainerApp2/Areas/GymMember/Controllers/TrainerContactController.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YourTrainerApp.Models;
+using YourTrainerApp.Services.IServices;
 
 namespace YourTrainerApp.Areas.GymMember.Controllers;
 
 [Area("GymMember")]
 public class TrainerContactController : Controller
 {
-	public IActionResult Index()
-	{
-		List<string> names = new() { "Paweł", "Kacper", "Michał", "Piotr", "Patryk", "Szymon" };
+	private readonly ITrainerDataService _trainerDataService;
 
-		return View(names);
+    public TrainerContactController(ITrainerDataService trainerDataService)
+    {
+		_trainerDataService = trainerDataService;
+    }
+
+    public async Task<IActionResult> Index()
+	{
+		APIResponse apiResponse = await _trainerDataService.GetAllAsync<APIResponse>();
+
+		List<TrainerDataModel> trainersData = JsonConvert.DeserializeObject<List<TrainerDataModel>>(Convert.ToString(apiResponse.Result));
+
+
+        List<string> names = new() { "Paweł", "Kacper", "Michał", "Piotr", "Patryk", "Szymon" };
+
+		return View(trainersData);
 	}
 
 	public IActionResult TrainerMessages()
--- YourTrainerApp2/Models/TrainerDataModel.cs
+++ YourTrainerApp2/Models/TrainerDataModel.cs
@@ -10,6 +10,8 @@ public class TrainerDataModel
 {
 	public int Id { get; set; }
 	public int TrainerId { get; set; }
+	// public string Name { get; set; }
+	// public float Rate { get; set; }
 	public string Description { get; set; }
 	public string Email { get; set; }
 	public string PhoneNumber { get; set; }
